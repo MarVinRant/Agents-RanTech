3a745c85676345267d0115ec245f5450e40b018e
Marcos Rantigueri <marcosranti@gmail.com>
2026-08-26 23:12:04 -0300

    feat: add organization detail view

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,8 @@
 import PlatformPage from "../platform-page";
+import OrganizationDetail from "../organization-detail";
 
-export default function Page() {
+export default async function Page({ params }: { params: Promise<{ slug?: string[] }> }) {
+  const { slug } = await params;
+  if (slug?.[0] === "organizations" && slug[1]) return <OrganizationDetail id={slug[1]} />;
   return <PlatformPage />;
 }
